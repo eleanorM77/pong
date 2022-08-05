f564fb153b362bbf16976f9ecf78161250a5a134
eleanormncll <110623741+eleanorM77@users.noreply.github.com>
2022-08-05 21:33:45 +0000

    started main function and created call of main

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ clock = pygame.time.Clock()
 
 #setting display
 def set_display():
+  global screen
   screen_info = pygame.display.Info()
   screen_width = screen_info.current_w
   screen_height = screen_info.current_h
@@ -23,6 +24,25 @@ def set_display():
 
 set_display()
 
+black = (0,0,0)
+
+def main():
+  #game loop
+  while True:
+    #handling quit event
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit
+      
+    #visuals
+    screen.fill(black)
+    #updating events
+    pygame.display.flip()
+    clock.tick(60)
 
 
+#call main
+if __name__ == '__main__':
+  main()
   
